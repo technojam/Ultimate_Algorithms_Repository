cc1dd743fe8f3f39b3d4f9e69924b63260c08a7a
Anju Kumari <42924677+Anju1415@users.noreply.github.com>
2020-09-03 01:38:44 +0530

    Update create_tree.h

--- tree/create_tree.h
+++ tree/create_tree.h
@@ -8,7 +8,7 @@ public:
 class create_tree{
 
 public:
-  static void add_root(Node** root,int data);
+  static void add_root(Node** root,int data); //Node* in place of Node**
   static void add_node(Node* root,int data);
 
 };
