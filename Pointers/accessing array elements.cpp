95e15d520f59685ac9ebf96d9b6598319ae217ae
Rishabh Kapoor <55848051+Rishabhkapoor@users.noreply.github.com>
2020-09-20 02:00:46 +0530

    Update accessing array elements.cpp

--- Pointers/accessing array elements.cpp
+++ Pointers/accessing array elements.cpp
@@ -5,7 +5,11 @@ using namespace std;
 int main()
 {
     int arr[] = { 10, 20, 30, 40 };
+    
+    // Compiler converts below to *(arr + 2). 
     cout << arr[2] <<endl;
+    
+    // So below also works.
     cout << *(arr + 2) << " ";
 
     return 0;
