966fe848cdb979dc91e63a12fc1dc6749aef61ad
HumanCompiler <55129013+saranshkhulbe7@users.noreply.github.com>
2020-10-01 02:31:27 +0530

    Create Check_whether_a_number_is_prime_or_not.cpp
    Check whether a number is prime or not.
    * Time Complexity O(√n)

---
+++ Basic Algorithms/Check_whether_a_number_is_prime_or_not.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+
+// isPrime() TAKES AN INTEGER INPUT AND PRINTS A STATEMENT ABOUT THE STATUS OF THE NUMBER BEING PRIME OR NOT.
+// TIME COMPLEXITY = O(√n).
+void isPrime(int number)
+{
+    cout<<"\n";
+    if(number<=1)
+    {
+        cout<<number<<" is not prime.";
+    }
+    else if(number == 2)
+    {
+        cout<<number<<" is prime.";
+    }
+    else
+    {
+        for(int i = 2; i*i <= number; i++)                  //KEY PART WHICH MAKES THIS ALGORITHM O(√n)
+        {
+            if(number%i==0)
+            {
+                cout<<number<<" is not prime.";
+                return;
+            }
+        }
+        cout<<number<<" is prime.";
+    }
+}
+
+
+int main()
+{
+    int number;
+    cout<<"Enter the number : ";
+    cin>>number;
+    isPrime(number);
+}
