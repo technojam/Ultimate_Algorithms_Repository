88d54127f33aa43779feee0d8ca445e6e827c99f
Shrikant991 <harshbhardwaj991@gmail.com>
2020-10-02 10:03:23 +0530

    Added Comments in bipartiteTest.cpp

--- Graphs/BipartiteTest.cpp
+++ Graphs/BipartiteTest.cpp
@@ -1,22 +1,31 @@
+/*
+
+ This algorithm is for Bipartite testing a graph i.e can the Graph be divided into two different sets of nodes
+ which have Union as NULL.
+
+ Each Node is given a color, if two adjacent nodes have same color then Bipartition is not possible
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> v[1000];
-bool m[1000];
-int l[1000];
+vector<int> v[1000]; // Adjacency List
+bool m[1000];        // Marking Vector
+int l[1000];         // Color Vector
 bool test(int n,int c)
 {
     m[n]=true;
-    l[n]=c;
+    l[n]=c;           // Color assignment
     for(auto j : v[n])
     {
         if(m[j]==false)
         {
-            if(test(j,c^1)==false)
+            if(test(j,c^1)==false)         // Recursive False returning
                 return false;
         }
         else
         {
-            if(l[j]==l[n])
+            if(l[j]==l[n])                 // Adjacent nodes having same color
                 return false;
         }
 
@@ -30,7 +39,7 @@ int main()
     for(i=0;i<n;i++)
     {
         cin >> a >> b;
-        v[a].push_back(b);
+        v[a].push_back(b);                  // making of adjacency list
         v[b].push_back(a);
     }
     if(!test(1,0))
