6b8dd34124565e84cc395fb509c481e918e2520a
hersheey <72225658+hersheey@users.noreply.github.com>
2020-10-02 16:08:05 +0530

    Create flipping_bitss.cpp

---
+++ flipping_bitss.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the flippingBits function below.
+long flippingBits(long n) {
+
+unsigned int q;
+q=~n;
+return q;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        long n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        long result = flippingBits(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
