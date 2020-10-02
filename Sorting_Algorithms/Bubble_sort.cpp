30a9750dac519799c0902671105ae11b6875f09c
Ayush Raj <beast@localhost.localdomain>
2020-10-02 13:13:41 +0530

    added some important Sorting Algorithm

---
+++ Sorting_Algorithms/Bubble_sort.cpp
@@ -0,0 +1,54 @@
+||--------------------||
+||     AYUSH RAJ      ||
+||--------------------||
+#include <stdio.h> 
+// function for swapping
+void swap(int *xp, int *yp) 
+{ 
+	int temp = *xp; 
+	*xp = *yp; 
+	*yp = temp; 
+} 
+
+//  Bubble Sort algorithm
+void bubbleSort(int arr[], int n) 
+{ 
+int i, j; 
+bool swapped; 
+for (i = 0; i < n-1; i++) 
+{ 
+	swapped = false; 
+	for (j = 0; j < n-i-1; j++) 
+	{ 
+		if (arr[j] > arr[j+1]) 
+		{ 
+		swap(&arr[j], &arr[j+1]); 
+		swapped = true; 
+		} 
+	} 
+
+	// IF no two elements were swapped by inner loop, then break 
+	if (swapped == false) 
+		break; 
+} 
+} 
+
+/* Function to print an array */
+void printArray(int arr[], int size) 
+{ 
+	int i; 
+	for (i=0; i < size; i++) 
+		printf("%d ", arr[i]); 
+	printf("n"); 
+} 
+
+int main() 
+{ 
+	int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+	int n = sizeof(arr)/sizeof(arr[0]); 
+	bubbleSort(arr, n); 
+	printf("Sorted array: \n"); 
+	printArray(arr, n); 
+	return 0; 
+} 
+
