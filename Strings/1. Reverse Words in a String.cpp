99444b2c9e342ce18a33023ec567f1d3a96d3a92
Rakshitha Rai <06rakshitha@gmail.com>
2020-10-02 09:55:43 +0530

    String program added to reverse words

---
+++ Strings/1. Reverse Words in a String.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+//prototype
+void reverse(char *start, char *end);
+ 
+/*Function to reverse words*/
+void reverseWords(char *s)
+{
+    char *word_begin = NULL;
+    char *temp = s; /* temp is for word boundry */
+ 
+   //Reversing each word
+    //Traversing the string
+    for(int i=0;i<strlen(s);i++ )
+    {
+        //string should have a proper start ie,word_begin, which is not space ie,temp[i] != ' '
+        if (( word_begin == NULL ) && (temp[i] != ' ') )
+        {
+            word_begin=(temp+i);
+        }
+        //Each word, reverse the word
+        //In below if condition words are considerd by seeing space 
+        if(word_begin && (temp[i+1] == ' ') || (temp[i+1] == '\0'))
+        {
+            reverse(word_begin, (temp+i));
+            word_begin = NULL;
+        }
+        
+    }
+ 
+    //Reversing the entire string
+    reverse(s, (temp+strlen(s)-1));
+}
+//this function will reverse the string, when given starting and ending pointers to the string
+void reverse(char *start, char *end)
+{
+  char temp;
+  while (start < end)
+  {
+    temp = *start;
+    *start++ = *end;
+    *end-- = temp;
+  }
+}
+ 
+// Driver function to test above functions 
+int main()
+{
+  char s[] = " Hello there!";
+  reverseWords(s);
+  cout<<s;
+  return 0;
+}
