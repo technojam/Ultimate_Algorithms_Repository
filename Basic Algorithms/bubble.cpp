9f08953e29a368ac258ea1c0db262621f225de26
Atul Kumar Singh <57915351+atulsingh2030@users.noreply.github.com>
2020-10-03 23:58:22 +0530

    Update bubble.cpp

--- Basic Algorithms/bubble.cpp
+++ Basic Algorithms/bubble.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-void swapping(int &a, int &b) {      
+void swapping(int &a, int &b) {   //swapping the content of a & b   
    int temp;
    temp = a;
    a = b;
@@ -13,9 +13,9 @@ void display(int *array, int size) {
 }
 void bubbleSort(int *array, int size) {
    for(int i = 0; i<size; i++) {
-      int swaps = 0;         
+      int swaps = 0;         //to detect any swap is there or not
       for(int j = 0; j<size-i-1; j++) {
-         if(array[j] > array[j+1]) {       
+         if(array[j] > array[j+1]) {      //when the item is greater than next 
             swapping(array[j], array[j+1]);
             swaps = 1;    
          }
@@ -28,7 +28,7 @@ int main() {
    int n;
    cout << "Enter the number of elements: ";
    cin >> n;
-   int arr[n];     
+   int arr[n];     //create an array with given no. of elements.
    cout << "Enter elements:" << endl;
    for(int i = 0; i<n; i++) {
       cin >> arr[i];
