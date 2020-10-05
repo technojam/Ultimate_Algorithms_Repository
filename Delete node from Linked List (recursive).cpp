ed525f917edbc09e191149781c9b6f897efdf8a2
Sakshi Thakur <nehathakursakshi@gmail.com>
2020-10-05 16:40:17 +0530

    Create Delete node from Linked List (recursive).cpp

---
+++ Delete node from Linked List (recursive).cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+
+Node *deleteNodeRec(Node *head, int pos) {
+    if(head==NULL){
+        return head;
+    }
+        
+    if(pos==0){
+        Node *a =head;
+        head=head->next;
+        delete a;
+        return head;
+    }
+    head->next = deleteNodeRec(head->next, pos-1);
+    return head;
+	
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		Node *head = takeinput();
+		int pos;
+		cin >> pos;
+		head = deleteNodeRec(head, pos);
+		print(head);
+	}
+
+	return 0;
+}
