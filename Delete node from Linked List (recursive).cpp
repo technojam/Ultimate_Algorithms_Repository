598ed15e015808d015b16ef82e40176895db058c
Sakshi Thakur <nehathakursakshi@gmail.com>
2020-10-08 10:43:29 +0530

    Update Delete node from Linked List (recursive).cpp
    Added comments

--- Delete node from Linked List (recursive).cpp
+++ Delete node from Linked List (recursive).cpp
@@ -1,66 +1,68 @@
 #include <iostream>
 using namespace std;
 
-class Node
+class Node                   		
 {
 public:
-	int data;
+	int data;                         //data in a node of linked list
 	Node *next;
-	Node(int data)
+	Node(int data)			// constructor to create the node
 	{
 		this->data = data;
-		this->next = NULL;
+		this->next = NULL;     // next pointer initially pointing to null
 	}
 };
 
 
-Node *takeinput()
+Node *takeinput()					// function to take input
 {
 	int data;
 	cin >> data;
-	Node *head = NULL, *tail = NULL;
-	while (data != -1)
+	Node *head = NULL, *tail = NULL;		//tail to kep track of the last node so that we don't have to traverse every time to add node
+	while (data != -1)				// check if data is entered 
 	{
 		Node *newNode = new Node(data);
-		if (head == NULL)
+		if (head == NULL)			// if head is null, it's the first entry. 
 		{
-			head = newNode;
-			tail = newNode;
+			head = newNode;			// initialise head with the newNode
+			tail = newNode;			// initialise tail with the newNode
 		}
 		else
 		{
-			tail->next = newNode;
-			tail = newNode;
+			tail->next = newNode;		//if head!=null, add newnode to tail->next
+			tail = newNode;			// move tail to newNode
 		}
 		cin >> data;
 	}
 	return head;
 }
 
-void print(Node *head)
+void print(Node *head)			//function to print the linked list
 {
 	Node *temp = head;
-	while (temp != NULL)
+	while (temp != NULL)			// print if head is not null
 	{
-		cout << temp->data << " ";
+		cout << temp->data<< " ";
 		temp = temp->next;
 	}
 	cout << endl;
 }
 
-
-Node *deleteNodeRec(Node *head, int pos) {
-    if(head==NULL){
+/*  the recursive function takes head and positon to delete the node, we call the function everytime with the next element 
+until the pos =0, when pos=0, we move head to next and delete the node that holds the value of previous head.
+*/
+Node *deleteNodeRec(Node *head, int pos) {		// recursive function to delete a node
+    if(head==NULL){					// if head is null, simply return
         return head;
     }
         
-    if(pos==0){
+    if(pos==0){						//if pos is 0, delete the head node		
         Node *a =head;
         head=head->next;
         delete a;
         return head;
     }
-    head->next = deleteNodeRec(head->next, pos-1);
+    head->next = deleteNodeRec(head->next, pos-1);   //call the function with next of head and pos-1
     return head;
 	
 }
