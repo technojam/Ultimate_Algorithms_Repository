8f075fc78499c7d7393ee1d86774644751d4b2bf
chiillax <ak0852122@gmail.com>
2020-10-10 13:44:28 +0530

    added Prim's algorithm

---
+++ Basic Algorithms/Prims_algorithm.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+vector<vector<int>> adj;    // adjacency matrix of graph
+const int INF = 1000000000; // weight INF means there is no edge
+
+struct Edge
+{
+    int w = INF, to = -1;
+};
+
+void prim()
+{
+    int total_weight = 0;
+    vector<bool> selected(n, false);
+    vector<Edge> min_e(n);
+    min_e[0].w = 0;
+
+    for (int i = 0; i < n; ++i)
+    {
+        int v = -1;
+        for (int j = 0; j < n; ++j)
+        {
+            if (!selected[j] && (v == -1 || min_e[j].w < min_e[v].w))
+                v = j;
+        }
+
+        if (min_e[v].w == INF)
+        {
+            cout << "No MST!" << endl;
+            exit(0);
+        }
+
+        selected[v] = true;
+        total_weight += min_e[v].w;
+        if (min_e[v].to != -1)
+            cout << v << " " << min_e[v].to << endl;
+
+        for (int to = 0; to < n; ++to)
+        {
+            if (adj[v][to] < min_e[to].w)
+                min_e[to] = {adj[v][to], v};
+        }
+    }
+
+    cout << total_weight << endl;
+}
