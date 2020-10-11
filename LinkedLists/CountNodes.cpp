98209ebf73915ef804c336f203636dcc094606d7
Shikhar Srivastava <60563576+shikhar8434@users.noreply.github.com>
2020-10-11 16:28:33 +0530

    added comments

--- LinkedLists/CountNodes.cpp
+++ LinkedLists/CountNodes.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-struct Node{
-    int data;
+struct Node{         // Node declaration
+    int data;   
     struct Node *next;
     
     Node(int x){
@@ -11,27 +11,27 @@ struct Node{
         }
 };
 
-int CountElements(Node *head){
+int CountElements(Node *head){    // Count function
     
     int count=0;
     
-    struct Node *curr= head;
-    while(curr!=NULL){
+    struct Node *curr= head;   // Creating a new pointer curr to store address of head
+    while(curr!=NULL){   //traversing till the end of linked list throught curr pointer
         curr=curr->next;
-        count++;
+        count++;  // incrementing count to count number of nodes traversed
     }
     return count;
 }
 
 int main()
 {
-    int data,n;
-    cin>>n;
+    int data,n;  
+    cin>>n;   // number of elements in linked list
     cin>>data;
     struct Node *head= new Node(data);
     struct Node *tail= head;
     for(int i=0;i<n-1;i++){
-        cin>>data;
+        cin>>data;   // taking input of values of linked list
         tail->next=new Node(data);
         tail=tail->next;
     }
