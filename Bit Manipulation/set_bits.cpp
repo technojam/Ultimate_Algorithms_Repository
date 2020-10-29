f5ac3c62ca4ceb0b1f7c3f32d10eb9f0836a03b4
iamatul45 <49286896+iamatul45@users.noreply.github.com>
2020-10-29 09:51:30 +0530

    added set_bits.cpp

---
+++ Bit Manipulation/set_bits.cpp
@@ -0,0 +1,24 @@
+// C++ program to Count set 
+// bits in an integer 
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+/* Function to get no of set bits in binary 
+representation of positive integer n */
+unsigned int countSetBits(unsigned int n) 
+{ 
+    unsigned int count = 0; 
+    while (n) { 
+        count += n & 1; 
+        n >>= 1; 
+    } 
+    return count; 
+} 
+  
+/* Program to test function countSetBits */
+int main() 
+{ 
+    int i = 9; 
+    cout << countSetBits(i); 
+    return 0; 
+}
